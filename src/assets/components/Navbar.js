e5782041dcf53809fe6d4bc9ced83ad70dcac5a5
Sajith <sajithanto598@gmail.com>
2026-03-31 16:45:58 +0530

    fix errors

--- src/assets/components/Navbar.js
+++ src/assets/components/Navbar.js
@@ -3,7 +3,7 @@ function Navbar(){
             <nav class="navbar">
         <h1 class="logo">Sajith's Garage</h1>
         <div class="nav-links">
-            <a href="">Home</a>
+            <a href="#home">Home</a>
             <a href="#about">About</a>
            <a href="#contact">Contact</a>
         </div>
